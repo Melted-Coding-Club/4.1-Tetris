5df8819b29864d8b3676368b1eb6d813ad6436ab
MeltedButetr77 <MeltedButterGaming@gmail.com>
2024-04-16 09:49:30 +0930

    Updated piece movement logic and collision detection in Tetris game. Fixed rendering issues with piece offsets and game location. Adjusted game area dimensions for better gameplay experience.

--- main.py
+++ main.py
@@ -20,41 +20,45 @@ class Piece:
                 [(1, 0), (1, 1), (0, 1), (0, 2), (7, 1)],
             ]
 
-        self.rects = []
-        for i in range(len(self.offsets[self.offset_num])):
-            x = self.game.grid_size * self.offsets[self.offset_num][i][0] + self.origin[0] + game.game_location[0]
-            y = self.game.grid_size * self.offsets[self.offset_num][i][1] + self.origin[1] + game.game_location[1]
-            rect = pygame.Rect(x, y, self.game.grid_size, self.game.grid_size)
-            self.rects.append(rect)
-
     def render(self):
-        for rect in self.rects:
-            pygame.draw.rect(self.game.screen, self.colour, rect)
+        for offset in self.offsets[self.offset_num]:
+            pygame.draw.rect(self.game.screen, self.colour, pygame.Rect(
+                self.origin[0] * self.game.grid_size + offset[0] * self.game.grid_size + self.game.game_location[0],
+                self.origin[1] * self.game.grid_size + offset[1] * self.game.grid_size + self.game.game_location[1],
+                self.game.grid_size,
+                self.game.grid_size
+            ))
 
     def update_rects(self):
+        blocks = []
         # returns True if a collision is made
-        for i, rect in enumerate(self.rects):
-            rect.x = self.game.grid_size * self.offsets[self.offset_num][i][0] + self.origin[0] + self.game.game_location[0]
-            rect.y = self.game.grid_size * self.offsets[self.offset_num][i][1] + self.origin[1] + self.game.game_location[1]
+        for offset in self.offsets[self.offset_num]:
+            blocks.append({
+                "location": (offset[0] + self.origin[0], offset[1] + self.origin[1]),
+                "colour": self.colour
+            })
 
-            if rect.bottom > self.game.game_location[1] + self.game.game_area[1]:
+        board_locations = []
+        for block in self.game.board:
+            board_locations.append(block["location"])
+
+        for block in blocks:
+            if block["location"][1] >= self.game.game_area[1]:
+                self.game.board.extend(blocks)
                 return True
 
-            for other_piece in self.game.pieces:
-                if other_piece == self:
-                    continue
-                for other_rect in other_piece.rects:
-                    if rect.colliderect(other_rect):
-                        return True
+            if block["location"] in board_locations:
+                self.game.board.extend(blocks)
+                return True
 
     def move(self, direction):
         origin = self.origin.copy()
         if direction == "down":
-            self.origin[1] += self.game.grid_size
+            self.origin[1] += 1
         elif direction == "left":
-            self.origin[0] -= self.game.grid_size
+            self.origin[0] -= 1
         elif direction == "right":
-            self.origin[0] += self.game.grid_size
+            self.origin[0] += 1
         # if collision is made, rollback changes and update the rects back to rollback state
         if self.update_rects():
             self.origin = origin
@@ -71,11 +75,13 @@ class Game:
 
         self.grid_size = 20
         self.game_location = (220, 40)
-        self.game_area = (10 * self.grid_size, 20 * self.grid_size)
+        self.game_area = (10, 20)
 
         self.pieces = [
             Piece(self),
         ]
+
+        self.board = []
 
         self.USEREVENT = 1
         pygame.time.set_timer(self.USEREVENT, 250)
@@ -103,7 +109,12 @@ class Game:
         self.screen.fill("white")
 
         # draw game area in game location
-        pygame.draw.rect(self.screen, "black", (self.game_location[0], self.game_location[1], self.game_area[0], self.game_area[1]))
+        pygame.draw.rect(self.screen, "black", (
+            self.game_location[0],
+            self.game_location[1],
+            self.game_area[0] * self.grid_size,
+            self.game_area[1] * self.grid_size
+        ))
         for piece in self.pieces:
             piece.render()
 
